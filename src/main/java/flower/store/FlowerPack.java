2841c94007b84e3f56472ea55f291d3320997bf9
Tetiana Shvets <shvets.pn@ucu.edu.ua>
2024-10-09 21:26:53 +0300

    implement classes

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,16 @@
 package flower.store;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class FlowerPack {
     private Flower flower;
-    private int quantity;
+    private int count;
+
+    public double getPrice() {
+        return flower.getPrice()*count;
+    }
 }
+
